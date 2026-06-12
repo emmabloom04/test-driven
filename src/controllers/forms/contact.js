fbbb9395df84c75a1f64938d3ed1855a1137b04c
emmabloom04 <blo23006@byui.edu>
2026-06-11 23:27:03 -0600

    adding a variables css file, importing, and adding contact specific styles

--- src/controllers/forms/contact.js
+++ src/controllers/forms/contact.js
@@ -6,7 +6,7 @@ const router = Router();
  * Display the contact form page.
  */
 const showContactForm = (req, res) => {
-    res.render('forms/contact/form', {
+    res.render('forms/contact', {
         title: 'Contact Us'
     });
 };
