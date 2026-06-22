d7b2c2b4efc8210cb7d76a49d580af9b08e532a4
emmabloom04 <blo23006@byui.edu>
2026-06-22 09:35:33 -0600

    removing rendering specific scripts, as it is not needed for this project thus far

--- src/middleware/global.js
+++ src/middleware/global.js
@@ -12,27 +12,14 @@
  */
 const setHeadAssetsFunctionality = (res) => {
     res.locals.styles = [];
-    res.locals.scripts = [];
 
     res.addStyle = (css, priority = 0) => {
         res.locals.styles.push({ content: css, priority });
     };
 
-    res.addScript = (js, priority = 0) => {
-        res.locals.scripts.push({ content: js, priority });
-    };
-
     // These functions will be available in EJS templates
     res.locals.renderStyles = () => {
         return res.locals.styles
-            // Sort by priority: higher numbers load first
-            .sort((a, b) => b.priority - a.priority)
-            .map(item => item.content)
-            .join('\n');
-    };
-
-    res.locals.renderScripts = () => {
-        return res.locals.scripts
             // Sort by priority: higher numbers load first
             .sort((a, b) => b.priority - a.priority)
             .map(item => item.content)
